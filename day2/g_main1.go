58648fab1ade87f8b4b4b4e7616400e2f96ba403
nishithshowri006 <nishithshowri006@gmail.com>
2024-12-02 12:12:24 +0530

    day2 part1 successful code

---
+++ day2/g_main1.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	// "sort"
+	"strconv"
+	"strings"
+)
+
+func check_increasing(level []int) bool {
+	i := 0
+	j := 1
+	for i < j && j < len(level) {
+		if level[j] > level[i] && level[j]-level[i] <= 3 && level[j]-level[i] >= 1 {
+			i++
+			j++
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func check_decreasing(level []int) bool {
+	i := 0
+	j := 1
+	for i < j && j < len(level) {
+		if level[j] < level[i] && level[i]-level[j] <= 3 && level[i]-level[j] >= 1 {
+			i++
+			j++
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func count_valid_levels(levels [][]int) int {
+	count := 0
+	for _, level := range levels {
+		if check_decreasing(level) || check_increasing(level) {
+			count++
+		}
+	}
+	return count
+}
+
+func strings_slice_int_slice(lists []string) []int {
+	ans := make([]int, len(lists))
+	var err error
+	for i := 0; i < len(lists); i++ {
+		ans[i], err = strconv.Atoi(lists[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return ans
+}
+func process_inputs(filename string) [][]int {
+	buffer, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer buffer.Close()
+	lines := bufio.NewScanner(buffer)
+	levels := make([][]int, 0)
+	for lines.Scan() {
+		level := strings.Fields(lines.Text())
+		levels = append(levels, strings_slice_int_slice(level))
+	}
+	return levels
+}
+func main() {
+	filename := "inputs.txt"
+	levels := process_inputs(filename)
+	ans := count_valid_levels(levels)
+	fmt.Println(ans)
+}
